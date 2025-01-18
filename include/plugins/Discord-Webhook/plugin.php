ada766ac0166c67788f2cefc29040118b3337cab
SkyDeckAGoGo <maggandgeez@gmail.com>
2025-01-18 18:52:52 +0000

    Implemented webhook plugin

---
+++ include/plugins/Discord-Webhook/plugin.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    'id'          => 'osticket:teams',
+    'version'     => '0.1',
+    'name'        => 'Discord Webhook notifier',
+    'author'      => 'Ivan Pavlović',
+    'description' => 'Notify Discord on new ticket.',
+    'url'         => 'https://github.com/ipavlovi/osTicket-Microsoft-Teams-plugin',
+    'plugin'      => 'teams.php:TeamsPlugin',
+);
